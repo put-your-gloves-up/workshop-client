2617af652da2356113ff0003ec95247ac48432d9
vincegobelins <vaguettaz@gmail.com>
2015-10-01 21:23:37 +0200

    adding spectral canvas

--- src/js/canvas/SpectralCanvas.js
+++ src/js/canvas/SpectralCanvas.js
@@ -9,12 +9,90 @@ export default class SpectralCanvas {
     constructor(canvas, detectedColors) {
         this.canvas = canvas;
         this.detectedColors = detectedColors;
+        this.bufferLength = 128;
+        this.init();
+    }
+
+    /**
+     * Init canvas and analyser
+     * @return {void}
+     */
+
+    init(){
+        this.visualizer = this.canvas
+        this.visualizerCtx = this.canvas.getContext("2d");
+
+        this.dataArray = new Uint8Array(this.bufferLength);
 
         this.render();
     }
 
-    render() {
+    /**
+     * Render the spectrum in canvas
+     * @return {void}
+     */
 
-        window.requestAnimationFrame(this.render);
+    render() {
+        this.width = this.visualizer.width;
+        this.height = this.visualizer.height;
+
+        // clean
+        this.visualizerCtx.clearRect(0, 0, this.width, this.height);
+
+        for(var color in this.detectedColors){
+
+            // get color
+            this.color = this.detectedColors[color].color;
+            this.colorRGB = this.convertToRGB(this.color);
+
+            // retrieve analyser and volume
+            this.analyser = this.detectedColors[color].sound.getAnalyser();
+            //console.log(this.detectedColors[color].color+' : ');
+            //console.log(this.detectedColors[color].sound.getVolume());
+            this.volume = this.detectedColors[color].sound.getVolume();
+
+            // get data
+            this.analyser.getByteFrequencyData(this.dataArray);
+
+            // draw
+            var barWidth = (this.width / this.bufferLength) * 2.5;
+            var barHeight;
+            var x = 0;
+
+            for(var i = 0; i < this.bufferLength; i++) {
+                barHeight = this.dataArray[i];
+
+                this.visualizerCtx.fillStyle = 'rgba(' + this.colorRGB.r + ',' + this.colorRGB.g + ',' + this.colorRGB.b + ', 0.6 )';
+                this.visualizerCtx.fillRect(x,(this.height-barHeight/2) * (1/this.volume), barWidth, (barHeight/2));
+
+                x += barWidth + 1;
+            }
+
+        }
+
+        this.drawVisual = requestAnimationFrame(this.render.bind(this));
+    }
+
+    /**
+     * Convert string color to RGB
+     * @return {void}
+     */
+
+    convertToRGB(color) {
+
+        var colors = {
+            red : {r : 255, g : 0, b: 0},
+            yellow : {r: 255, g: 255, b: 50},
+            magenta : {r: 124, g: 0, b: 125}
+        }
+
+        if(typeof colors[color.toLowerCase()] != 'undefined') {
+            return colors[color.toLowerCase()];
+        }
+
+        else {
+            return false;
+        }
+
     }
 }
